10a238000f7d736c063fe53d7c6dd3d248f562d1
popcorn1939 <73425312+popcorn1939@users.noreply.github.com>
2023-09-25 19:38:03 +0000

    123

--- pages/6_test.py
+++ pages/6_test.py
@@ -3,6 +3,7 @@ import gpxpy
 import gpxpy.gpx
 import matplotlib.pyplot as plt
 import folium
+from streamlit_folium import st_folium, folium_static
 
 st.title("🦜🔗 Langchain Quickstart App 123456789")
 st.write("skrevet fra vsc september")
@@ -26,3 +27,29 @@ plt.grid()
 # Show the plot
 #plt.show()
 st.pyplot(plt)
+
+
+
+
+
+
+class GPXRoutePlotter:
+    def __init__(self, gpx_file_path):
+        self.gpx_file_path = gpx_file_path
+        self.route = self.load_gpx_route()
+
+    def load_gpx_route(self):
+        gpx_file = open(self.gpx_file_path, 'r')
+        gpx = gpxpy.parse(gpx_file)
+        return [(point.latitude, point.longitude) for point in gpx.tracks[0].segments[0].points]
+
+    def plot_route(self):
+        m = folium.Map(location=[self.route[0][0], self.route[0][1]], zoom_start=10)
+        folium.PolyLine(locations=self.route, color='blue').add_to(m)
+        return m
+
+
+gpx_plotter = GPXRoutePlotter('datafiles/test.gpx')
+route_map = gpx_plotter.plot_route()
+st_data = st_folium(route_map, width=700)
+
