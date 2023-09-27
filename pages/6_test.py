3398f08d3cca8ab49f23c8afb559189c4d4cff42
popcorn1939 <73425312+popcorn1939@users.noreply.github.com>
2023-09-27 12:27:28 +0000

    123

--- pages/6_test.py
+++ pages/6_test.py
@@ -4,6 +4,8 @@ import gpxpy.gpx
 import matplotlib.pyplot as plt
 import folium
 from streamlit_folium import st_folium, folium_static
+from io import BytesIO
+import base64
 
 st.title("🦜🔗 Langchain Quickstart App 123456789")
 st.write("skrevet fra vsc september")
@@ -33,65 +35,62 @@ st.pyplot(plt)
 
 
 
+
 class GPXRoutePlotter:
     def __init__(self, gpx_file_path):
         self.gpx_file_path = gpx_file_path
         self.route = self.load_gpx_route()
+        self.elevation_data = self.extract_elevation_data()
 
     def load_gpx_route(self):
         gpx_file = open(self.gpx_file_path, 'r')
         gpx = gpxpy.parse(gpx_file)
-        return [(point.latitude, point.longitude, point.elevation) for point in gpx.tracks[0].segments[0].points]
+        return [(point.latitude, point.longitude) for point in gpx.tracks[0].segments[0].points]
+
+    def extract_elevation_data(self):
+        gpx_file = open(self.gpx_file_path, 'r')
+        gpx = gpxpy.parse(gpx_file)
+        elevation_data = [point.elevation for point in gpx.tracks[0].segments[0].points]
+        return elevation_data
 
     def plot_route(self):
         m = folium.Map(location=[self.route[0][0], self.route[0][1]], zoom_start=10)
-        folium.PolyLine(locations=[(lat, lon) for lat, lon, _ in self.route], color='blue').add_to(m)
+        folium.PolyLine(locations=self.route, color='blue').add_to(m)
         return m
+    
+    def save_route_image(self, file_path):
+        route_map = self.plot_route()
+        route_map.save(file_path)
 
     def plot_elevation_profile(self):
-        elevations = [elevation for _, _, elevation in self.route]
-        distances = [0]
-        total_distance = 0
-
-        for i in range(1, len(self.route)):
-            lat1, lon1, _ = self.route[i - 1]
-            lat2, lon2, _ = self.route[i]
-            distance = self.haversine(lat1, lon1, lat2, lon2)
-            total_distance += distance
-            distances.append(total_distance)
-
-        plt.plot(distances, elevations)
-        plt.xlabel('Distance (m)')
-        plt.ylabel('Elevation (m)')
+        plt.figure(figsize=(8, 4))
+        plt.plot(range(len(self.elevation_data)), self.elevation_data, color='green')
+        plt.xlabel('Distance (Points)')
+        plt.ylabel('Elevation (meters)')
         plt.title('Elevation Profile')
-        plt.grid(True)
-
-    @staticmethod
-    def haversine(lat1, lon1, lat2, lon2):
-        # Calculate the distance between two points on the Earth's surface using the haversine formula
-        import math
-        radius = 6371000  # Earth radius in meters
-        dlat = math.radians(lat2 - lat1)
-        dlon = math.radians(lon2 - lon1)
-        a = math.sin(dlat/2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon/2) ** 2
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-        return radius * c
 
 
- 
+        # Save the elevation plot as an image
+        img_stream = BytesIO()
+        plt.savefig(img_stream, format='png')
+        img_stream.seek(0)
 
-    # Plot the elevation profile
-    #gpx_plotter.plot_elevation_profile()
-    #plt.show()
+        # Display the elevation plot as an image in Streamlit
+        st.image(img_stream, caption='Elevation Profile', use_column_width=True)
 
 
-    gpx_plotter = GPXRoutePlotter('datafiles/test.gpx')
+# Streamlit app
+st.title('GPX Route Plotter with Elevation Profile')
 
-   # Plot the route on the map
-    route_map = gpx_plotter.plot_route()
-    route_map.save('route_map.html')
-    st_data = st_folium(route_map, width=700)
+gpx_file_path = 'datafiles/test.gpx'  # Modify the path accordingly
+gpx_plotter = GPXRoutePlotter(gpx_file_path)
 
-    # Plot the elevation profile
-    st.pyplot(gpx_plotter)
-    st.pyplot(plt)
+st.subheader('Route Map')
+m = gpx_plotter.plot_route()
+st_data = st_folium(m, width=700)
+#st.write(route_map._repr_html_(), unsafe_allow_html=True)
+
+st.subheader('Elevation Profile')
+gpx_plotter.plot_elevation_profile()
+
+gpx_plotter.save_route_image('route_map.jpg')
