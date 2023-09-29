5905050845068db5d21950302513ced50be3d1bd
popcorn1939 <73425312+popcorn1939@users.noreply.github.com>
2023-09-29 12:39:14 +0000

    1234sdfsdf

--- pages/7_png.py
+++ pages/7_png.py
@@ -9,18 +9,19 @@ st.title("Folium Map in Streamlit with PIL")
 
 # Create a function to capture the Folium map as an image using time delay
 def capture_folium_map(map):
+    st.write("hej")
     # Create a temporary HTML file to save the Folium map
-    map.save("oslo_map.html")
+    #map.save("oslo_map.html")
 
     # Set up a Streamlit iframe to display the HTML file
-    st.components.v1.iframe("oslo_map.html", height=600)
+    #st.components.v1.iframe("oslo_map.html", height=600)
 
     # Wait for the iframe to load (adjust the delay if needed)
-    time.sleep(5)
+    #time.sleep(5)
 
     # Take a screenshot of the Streamlit app using the browser's print screen
-    st.image("screenshot.png")
-
+    #st.image("datafiles/screenshot.png")
+    #st.write("done")
 # Create a map centered at Oslo, Norway
 oslo_map = folium.Map(location=[59.9139, 10.7522], zoom_start=14)
 
